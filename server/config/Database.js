90806778c94934266aeb5cb5040bf52500600284
MuhammadFahrial <rialjhe111@gmail.com>
2025-11-06 12:57:02 +0800

    update

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
